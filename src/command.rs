f6ad7064aeb29a88c8428db2c53094d3f33eb1cb
Akiomi Kamakura <akiomik@gmail.com>
2025-12-27 00:39:18 +0900

    feat: update command

--- src/command.rs
+++ src/command.rs
@@ -1,20 +1,38 @@
-use futures::{stream::BoxStream, FutureExt, StreamExt};
+use futures::{
+    FutureExt, StreamExt,
+    stream::{self, BoxStream},
+};
 
-pub struct Command<M> {
-    pub(super) stream: Option<BoxStream<'static, M>>,
+pub enum Action<Msg> {
+    Message(Msg),
+    Quit,
 }
 
-impl<M> Command<M> {
+pub struct Command<Msg: Send + 'static> {
+    pub(super) stream: Option<BoxStream<'static, Action<Msg>>>,
+}
+
+impl<Msg: Send + 'static> Command<Msg> {
     pub fn none() -> Self {
         Self { stream: None }
     }
 
     pub fn perform<A>(
         future: impl Future<Output = A> + Send + 'static,
-        f: impl FnOnce(A) -> M + Send + 'static,
-    ) -> Command<M> {
+        f: impl FnOnce(A) -> Msg + Send + 'static,
+    ) -> Self {
+        Self::future(future.map(f))
+    }
+
+    pub fn future(future: impl Future<Output = Msg> + Send + 'static) -> Self {
         Self {
-            stream: Some(future.map(f).into_stream().boxed()),
+            stream: Some(future.into_stream().map(Action::Message).boxed()),
+        }
+    }
+
+    pub fn effect(action: Action<Msg>) -> Self {
+        Self {
+            stream: Some(stream::once(async move { action }).boxed()),
         }
     }
 }
--- src/runtime.rs
+++ src/runtime.rs
@@ -5,7 +5,7 @@ use futures::stream::StreamExt;
 use ratatui::prelude::Backend;
 use tokio::{sync::mpsc, time::sleep};
 
-use crate::{application::Application, subscription::SubscriptionManager};
+use crate::{application::Application, command::Action, subscription::SubscriptionManager};
 
 #[repr(transparent)]
 struct Instance<A: Application> {
@@ -14,34 +14,47 @@ struct Instance<A: Application> {
 
 pub struct Runtime<A: Application> {
     app: Instance<A>,
-    tx: mpsc::UnboundedSender<A::Message>,
-    rx: mpsc::UnboundedReceiver<A::Message>,
+    msg_tx: mpsc::UnboundedSender<A::Message>,
+    msg_rx: mpsc::UnboundedReceiver<A::Message>,
+    quit_tx: mpsc::Sender<()>,
+    quit_rx: mpsc::Receiver<()>,
     subscription_manager: SubscriptionManager<A::Message>,
 }
 
 impl<A: Application> Runtime<A> {
     pub fn new(app: A) -> Self {
-        let (tx, rx) = mpsc::unbounded_channel();
+        let (msg_tx, msg_rx) = mpsc::unbounded_channel();
+        let (quit_tx, quit_rx) = mpsc::channel(1);
         let instance = Instance { inner: app };
-        let subscription_manager = SubscriptionManager::new(tx.clone());
+        let subscription_manager = SubscriptionManager::new(msg_tx.clone());
 
         Self {
             app: instance,
-            tx,
-            rx,
+            msg_tx,
+            msg_rx,
+            quit_tx,
+            quit_rx,
             subscription_manager,
         }
     }
 
-    async fn process_messages(&mut self) {
-        while let Ok(msg) = self.rx.try_recv() {
+    fn process_messages(&mut self) {
+        while let Ok(msg) = self.msg_rx.try_recv() {
             let cmd = self.app.inner.update(msg);
 
-            let tx_clone = self.tx.clone();
+            let msg_tx = self.msg_tx.clone();
+            let quit_tx = self.quit_tx.clone();
             if let Some(mut stream) = cmd.stream {
                 tokio::spawn(async move {
-                    if let Some(value) = stream.next().await {
-                        let _ = tx_clone.send(value);
+                    if let Some(action) = stream.next().await {
+                        match action {
+                            Action::Message(msg) => {
+                                let _ = msg_tx.send(msg);
+                            }
+                            Action::Quit => {
+                                let _ = quit_tx.send(()).await;
+                            }
+                        }
                     }
                 });
             }
@@ -67,14 +80,18 @@ impl<A: Application> Runtime<A> {
                 self.app.inner.view(frame);
             })?;
 
-            self.process_messages().await;
+            self.process_messages();
+
+            if self.quit_rx.try_recv().is_ok() {
+                break;
+            }
 
             sleep(frame_duration).await;
         }
 
-        // // cancel all subscriptions
-        // self.subscription_manager.shutdown();
-        //
-        // Ok(())
+        // cancel all subscriptions
+        self.subscription_manager.shutdown();
+
+        Ok(())
     }
 }
